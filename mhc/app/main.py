644faa53274cee006e04cf4337e183e686e6e10a
ABX9801 <anurag.bhardwaj9801@gmail.com>
2024-11-16 13:55:39 +0530

    created api-server

--- mhc/app/main.py
+++ mhc/app/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 
-from app.api.api_v1.api import api_router
-from app.core.config import settings
 
 
 def create_app():
     app = FastAPI(
         title="Aaradhya"
     )
-
-    app.include_router(api_router, prefix=settings.API_V1_STR)
 
     return app
 
