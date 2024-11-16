7eaf7070620107bcce9c100a0f5461e29627e46b
ABX9801 <anurag.bhardwaj9801@gmail.com>
2024-11-17 00:07:41 +0530

    fix small stylistic issues

--- mhc/app/api/version1/api_router.py
+++ mhc/app/api/version1/api_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.version1.user import router as user_router
 from app.api.version1.chat import router as chat_router
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 
 router.include_router(user_router)
 router.include_router(chat_router)
--- mhc/app/main.py
+++ mhc/app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app.api.version1.api_router import router
+from starlette.middleware.cors import CORSMiddleware
 
 def create_app():
     app = FastAPI(
         title="Aaradhya"
     )
     app.include_router(router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
 
